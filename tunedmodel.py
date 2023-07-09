9d1dbe20bd7cf57a6437cdd206885d09db99728a
asunder123 <anand.sunder@capgemini.com>
2023-07-09 16:21:48 +0000

    Changes to UI rendering

--- tunedmodel.py
+++ tunedmodel.py
@@ -3,20 +3,24 @@ from vertexai.preview.language_models import TextGenerationModel
 
 app = Flask(__name__)
 
+def generate_response(prompt):
+    parameters = {
+        "temperature": 0.2,
+        "max_output_tokens": 256,
+        "top_p": 0.8,
+        "top_k": 40
+    }
+    model = TextGenerationModel.from_pretrained("text-bison@001")
+    model = model.get_tuned_model("projects/280303677176/locations/us-central1/models/7578953441469267968")
+    response = model.predict(prompt, **parameters)
+    return response.text
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         prompt = request.form['prompt']
-        parameters = {
-            "temperature": 0.2,
-            "max_output_tokens": 256,
-            "top_p": 0.8,
-            "top_k": 40
-        }
-        model = TextGenerationModel.from_pretrained("text-bison@001")
-        model = model.get_tuned_model("projects/280303677176/locations/us-central1/models/7578953441469267968")
-        response = model.predict(prompt, **parameters)
-        return jsonify({"response": response.text})
+        response_text = generate_response(prompt)
+        return render_template('index.html', response_text=response_text)
     return render_template('index.html')
 
 if __name__ == '__main__':
